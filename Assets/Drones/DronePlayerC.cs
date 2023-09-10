e6dfab71c3eef3206b383b345cbb1e147ac01fc1
TaurusCommit91 <105094991+TaurusCommit91@users.noreply.github.com>
2023-09-10 12:38:37 -0400

    Updated Changes to DroneShow + Added Script/Delay Node

--- Assets/Drones/DronePlayerC.cs
+++ Assets/Drones/DronePlayerC.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drone : MonoBehaviour
+public class DronePlayerC : MonoBehaviour
 {
-    public Animator DronePlayerC;
-    public float delay = 0.5f;
+    private Animator animator;
+    public float animationDelay = 1.0f; // Adjust the delay time for each drone
 
-    // Start is called before the first frame update
     void Start()
     {
-        Invoke("StartAnimation", delay);
-        
+        animator = GetComponent<Animator>(); // Get the Animator component
+
+        // Set the AnimationDelay parameter to control the delay before animation starts
+        animator.SetFloat("AnimationDelay", animationDelay);
+
+        // Trigger the animation when the delay time elapses
+        StartCoroutine(TriggerAnimation());
     }
 
-    void StartAnimation()
+    IEnumerator TriggerAnimation()
     {
-        DronePlayerC.SetTrigger("NextDrone");
+        // Wait for the specified animation delay
+        yield return new WaitForSeconds(animationDelay);
+
+        // Trigger the animation by setting the parameter
+        animator.SetBool("PlayAnimation", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // You can add any other update logic here if needed
     }
 }
